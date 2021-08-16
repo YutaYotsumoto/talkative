f107fac0e64665b9dc9633adfbc4e5b98380f011
Yuta Yotsumoto <y.2000529@gmail.com>
2021-08-16 10:52:10 +0900

    ディレクトリ整理

---
+++ src/Screen/Screen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, StyleSheet} from 'react-native';
+
+const Screen = () => {
+  return (
+    <SafeAreaView>
+      <Text>ヤッホーーーーーー</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Screen;
